7a36898b1dd7573bbcd3101b7deaad074c15925d
Artem Prygin <a.prygin@wellyes.ru>
2021-11-21 22:48:05 +0300

    in progress

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,8 +2,8 @@ import fs from 'fs/promises';
 import path from 'path';
 import os from 'os';
 import nock from 'nock';
-import pageLoader from '../index.js';
 import { expect } from '@jest/globals';
+import pageLoader from '../index.js';
 
 const getFixturePath = (filename) => path.join(path.resolve(), '__fixtures__', filename);
 
@@ -13,7 +13,7 @@ describe('page-loader', () => {
 	beforeEach(async () => {
 		outputFolder = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
 	});
-	
+
 	test('page loaded', async () => {
 		const nockData = await fs.readFile(getFixturePath('courses.html'), 'utf-8');
 		const scope = nock('https://blablabla.ru')
@@ -21,7 +21,7 @@ describe('page-loader', () => {
 			.reply(200, nockData);
 		await pageLoader('https://blablabla.ru/courses', outputFolder);
 		const page = await fs.lstat(path.join(outputFolder, 'blablabla-ru-courses.html'));
-		
+
 		expect(scope.isDone()).toBe(true);
 		expect(page.isFile()).toBe(true);
 	});
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 import abc from './write-file.js';
 
-export default (link, dir) => {
-	return axios.get(link)
-		.then((res) => abc(link, dir, res.data))
-		.then((res) => res)
-		.catch((e) => console.log(e));
-};
+export default (link, dir) => axios.get(link)
+	.then((res) => abc(link, dir, res.data))
+	.then((res) => res)
+	.catch((e) => console.log(e));
