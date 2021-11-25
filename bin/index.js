747f5d25bd79e3a93aed2d30a0071552472418b9
Artem Prygin <a.prygin@wellyes.ru>
2021-11-25 11:23:44 +0300

    catch block delete in bin/index.js

--- bin/index.js
+++ bin/index.js
@@ -6,6 +6,7 @@ program.version('1.0.0')
 	.arguments('<link>')
 	.option('-o, --output [dir]', 'output dir', process.cwd())
 	.action((link) => pageLoader(link, program.output)
-		.then((data) => data && console.log(data))
-		.catch((e) => console.log(e.message)))
+		.then((data) => {
+			console.log(data);
+		}))
 	.parse(process.argv);
